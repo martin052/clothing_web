e6e4fc9772a49d671a516d49dde089ea98e133f9
Martin Trembac <martintrembac052@gmail.com>
2023-11-18 14:42:19 +0000

    added styles

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,18 +1,14 @@
+import DirectoryItem from "../directory-item/directory-item.component";
+import "./directory.styles.scss";
 
-import CategoryItem from '../category-item/category-item.component';
-import './directory.styles.scss';
-
-const Directory = ({categories}) => {
-    return (
-        <div className="directory-container">
-            {categories.map((category) => (
-                <CategoryItem 
-                key={category.id}
-                 category={category}
-                 ></CategoryItem>
-            ))}
-        </div>
-    )
-}
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category}></DirectoryItem>
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
